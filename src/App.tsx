f79fac7334ac9f88a36baad791d6f026f429b3ec
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-06-22 16:11:53 +0530

    (init): add cms provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,55 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TinaCMS, TinaCMSProvider, useCMS } from "tinacms";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cms = new TinaCMS({
+    sidebar: true,
+  });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <TinaCMSProvider cms={cms}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
+          <ToggleButton />
+          <p>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {" | "}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+      </div>
+    </TinaCMSProvider>
+  );
 }
 
-export default App
+export default App;
+
+const ToggleButton: React.FC = () => {
+  const cms = useCMS();
+  return (
+    <button type="button" onClick={() => cms.toggle()}>
+      {cms.enabled ? "Exit Edit Mode" : "Edit This Site"}
+    </button>
+  );
+};
