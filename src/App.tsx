de78649ade7b1482ccbaf7cbb1ee7622ae664467
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-06-22 16:28:38 +0530

    (feat): add FormConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { TinaCMS, TinaProvider, useCMS } from "tinacms";
+import { TinaCMS, TinaProvider, useCMS, useForm, usePlugin } from "tinacms";
 
 function App() {
   const [cms] = React.useState(() => {
@@ -13,26 +13,48 @@ function App() {
 
   return (
     <TinaProvider cms={cms} position="displace">
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{PageData.title}</p>
-          <ToggleButton />
-          <p className="App-link">{PageData.body}</p>
-        </header>
-      </div>
+      <PageContent />
     </TinaProvider>
   );
 }
 
 export default App;
 
-const ToggleButton: React.FC = () => {
+const PageContent: React.FC = () => {
   const toggle = useCMS();
+  const formConfig = {
+    id: "tina-tutorial-index",
+    label: "Edit Page",
+    fields: [
+      {
+        name: "title",
+        label: "Title",
+        component: "text",
+      },
+      {
+        name: "body",
+        label: "Body",
+        component: "textarea",
+      },
+    ],
+    initialValues: PageData,
+    onSubmit: async () => {
+      window.alert("Saved!");
+    },
+  };
+  const [editableData, form] = useForm(formConfig);
+  usePlugin(form);
   return (
-    <button type="button" onClick={() => toggle.toggle()}>
-      {toggle.enabled ? "Exit Edit Mode" : "Edit This Site"}
-    </button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{editableData.title}</p>
+        <button type="button" onClick={() => toggle.toggle()}>
+          {toggle.enabled ? "Exit Edit Mode" : "Edit This Site"}
+        </button>
+        <p className="App-link">{editableData.body}</p>
+      </header>
+    </div>
   );
 };
 
